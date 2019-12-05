f2e63d21433261c9475beed1949161ded3042de1
Jake <lejake888@gmail.com>
2019-12-05 22:40:31 +0000

    Added menu, default/customise, number of guesses increments

--- game.py
+++ game.py
@@ -1,19 +1,34 @@
 import random
 
 flag = True
-noOfGuesses = 0
-randomNumber = random.randint(0,100)
+guesses = 0
 
-def randomiser(noOfGuesses):
-    guess = int(input("Guess a number: "))
-    if guess > randomNumber:
-        print("Lower")
-    elif guess < randomNumber:
-        print("Higher")
-    else:
-        print("You win!")
-    noOfGuesses = noOfGuesses + 1
-    print("Number of guesses: ", noOfGuesses)
+def menu():
+    choice = int(input("Welcome to 'Higher or Lower': 1- Default 2- Custom "))
+    if choice == 1:
+        randomNumber = random.randint(0,100)
+    elif choice == 2: 
+        randomNumber = setUp()
+    guess(guesses, randomNumber)
 
-while flag == True:
-    randomiser(noOfGuesses)
+def setUp():
+    lower = int(input("Set the smallest number: "))
+    higher = int(input("Set the highest number: "))
+    randomNumber = random.randint(lower,higher)
+    return(randomNumber)
+
+def guess(noOfGuesses, randomNumber):
+    flag = True
+    while flag:
+        guess = int(input("Guess a number: "))
+        if guess > randomNumber:
+            print("Lower")
+        elif guess < randomNumber:
+            print("Higher")
+        else:
+            print("You win!")
+            flag = False
+        noOfGuesses = noOfGuesses + 1
+        print("Number of guesses: ", noOfGuesses)
+
+menu()
